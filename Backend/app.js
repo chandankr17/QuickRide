597bcbb24017be3eb1fa27fa183d7a208038221f
chandankr17 <ck771677@gmail.com>
2025-12-19 18:20:42 +0530

    Updated code

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
 const dotenv=require('dotenv');
 dotenv.config();
 
+const cors = require('cors');
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const connectToDb = require('./db/db');
+connectToDb();
 
 app.use(cors());
 
