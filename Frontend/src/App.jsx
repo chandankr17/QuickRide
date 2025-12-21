3b73ab23071b46d556ec222cac376e404a0a3d80
chandankr17 <ck771677@gmail.com>
2025-12-21 16:54:01 +0530

    Updated frontend and configuration

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Start />} />
+        <Route path='/login' element={<UserLogin />} />
+        <Route path='/riding' element={<Riding />} />
+        <Route path='/captain-riding' element={<CaptainRiding />} />
+        <Route path='/signup' element={<UserSignup />} />
+        <Route path='/captain-login' element={<Captainlogin />} />
+        <Route path='/captain-signup' element={<CaptainSignup />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <div className='h-screen pt-8 flex justify-between flex-col w-full bg-red-400'>
+                <img className='w-15 ml-8' src="https://www.zopoyo.in/assets/stores/1564745295_quickride_jul.jpg" alt="QuickRide Logo"></img>
+                <div className='bg-white'>
+                    <h2>Ride with QuickRide</h2>
+                    <button>Continue</button>
+                </div>
+            </div>
+        </div>
+    )
+}
