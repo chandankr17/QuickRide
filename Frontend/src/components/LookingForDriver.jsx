0a0b668fa829dee7f712cad9deb92281302a852b
chandankr17 <ck771677@gmail.com>
2025-12-24 01:07:30 +0530

    Update project

--- Frontend/src/components/LookingForDriver.jsx
+++ Frontend/src/components/LookingForDriver.jsx
@@ -6,10 +6,10 @@ const LookingForDriver = (props) => {
             <h5 className='p-1 text-center w-[93%] absolute top-0' onClick={() => {
                 props.setVehicleFound(false)
             }}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
-            <h3 className='text-2xl font-semibold mb-5'>Looking for a Driver</h3>
+            <h3 className='text-2xl font-semibold mb-5'>Looking for Quick Captain</h3>
 
             <div className='flex gap-2 justify-between flex-col items-center'>
-                <img className='h-20' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
+                <img className='h-30' src="/car.png" alt="" />
                 <div className='w-full mt-5'>
                     <div className='flex items-center gap-5 p-3 border-b-2'>
                         <i className="ri-map-pin-user-fill"></i>
--- Frontend/src/components/WaitingForDriver.jsx
+++ Frontend/src/components/WaitingForDriver.jsx
@@ -8,11 +8,11 @@ const WaitingForDriver = (props) => {
       }}><i className="text-3xl text-gray-200 ri-arrow-down-wide-line"></i></h5>
 
       <div className='flex items-center justify-between'>
-        <img className='h-12' src="https://swyft.pl/wp-content/uploads/2023/05/how-many-people-can-a-uberx-take.jpg" alt="" />
+        <img className='h-15' src="/car.png" alt="" />
         <div className='text-right'>
           <h2 className='text-lg font-medium capitalize'>{props.ride?.captain.fullname.firstname}</h2>
           <h4 className='text-xl font-semibold -mt-1 -mb-1'>{props.ride?.captain.vehicle.plate}</h4>
-          <p className='text-sm text-gray-600'>Maruti Suzuki Alto</p>
+          <p className='text-sm text-gray-600'>Swift Dzire</p>
           <h1 className='text-lg font-semibold'>  {props.ride?.otp} </h1>
         </div>
       </div>
